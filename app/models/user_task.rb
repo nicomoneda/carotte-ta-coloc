d4fc4675172152618dfe7ea5ebcd234fac3ab583
Nicolas BRAUN <nico.braun@free.fr>
2021-06-01 15:19:57 +0200

    Move def methods before validations

--- app/models/user_task.rb
+++ app/models/user_task.rb
@@ -2,11 +2,12 @@ class UserTask < ApplicationRecord
   belongs_to :user
   belongs_to :tasks
 
-  validates task_status, presence: :true
-
   def task_status
     if :status.present? && :status == "Fait"
       errors.add(:until_at, "Well done :)")
     end
   end
+  
+  validates task_status, presence: :true
+
 end
