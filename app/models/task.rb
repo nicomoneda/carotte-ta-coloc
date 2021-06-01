15c93ef1d6aa3e819bcd929d7dc6ad6d6dabba44
Lucas Boitier <lboitier1@gmail.com>
2021-06-01 16:02:34 +0200

    Task Controller

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,7 +5,7 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :points, inclusion: { in: (1..200) }
   validates :points, presence: true
-  validates :carotte_card_description, presence: true
-  validates :carotte_card_points, presence: true
-  validates :carotte_card_points, inclusion: { in: (1..200) }
+  # validates :carotte_card_description, presence: true
+  # validates :carotte_card_points, presence: true
+  # validates :carotte_card_points, inclusion: { in: (1..200) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  resources :tasks, only: [:index, :create, :new]
+  resources :tasks, only: [:index, :create, :new, :show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
