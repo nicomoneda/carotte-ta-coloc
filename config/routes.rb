c3221e018075bd830f984e8d59a63282ff1993d5
Nicolas BRAUN <nico.braun@free.fr>
2021-06-01 12:16:19 +0200

    Root to sign_in

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     passwords: 'users/passwords'
   }
-  
-  root to: 'pages#home'
+
+  root to: 'sessions#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
