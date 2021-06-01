a98a2d37ee101f8e9474b59fbb15acf4aa48ea52
Nicolas BRAUN <nico.braun@free.fr>
2021-06-01 15:41:11 +0200

    Remove validates

--- app/models/carotte_card.rb
+++ app/models/carotte_card.rb
@@ -2,12 +2,5 @@ class CarotteCard < ApplicationRecord
   belongs_to :user
   belongs_to :tasks
 
-  def expiration_week_on
-    if :week_on.present? && :week_on < Date.today
-      errors.add(:week_on, "can't be in the past")
-    end
-  end
-  
-  validates expiration_week_on, presence: :true
 
 end
