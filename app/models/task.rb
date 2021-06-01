dc5b79ee939887b343f1cf59dbf036793c61b506
Lucas Boitier <lboitier1@gmail.com>
2021-06-01 14:45:24 +0200

    Start Task Controller, Index Method

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ApplicationRecord
   belongs_to :coloc
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates_inclusion_of :points, :inclusion => 1..100
+  validates :points, presence: true
+  validates :carotte_card_description, presence: true
+  validates :carotte_card_points, presence: true
+  validates_inclusion_of :carotte_card_points, :inclusion => 1..100
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   }
 
   root to: 'sessions#new'
+
+  resources :tasks, only: [:index, :create, :new]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
