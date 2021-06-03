49d472e562e33f814299ff42083f1b3c87169cc4
Nicolas BRAUN <nico.braun@free.fr>
2021-06-03 17:01:09 +0200

    Add carotted_task

--- app/controllers/user_tasks_controller.rb
+++ app/controllers/user_tasks_controller.rb
@@ -19,6 +19,22 @@ class UserTasksController < ApplicationController
     redirect_to root_path
   end
 
+  def carotte_task
+    @user_task = UserTask.find(params[:id])
+    @user = @user_task.user
+    @task = @user_task.task
+
+    if @user.current_points >= @task.carotte_card_points
+      other_users = current_coloc.users -  @user  
+      carotted_user = other_users.sample
+      @user_task.update(user: carotted_user)
+      
+      carotted_points = @user.current_points - @task.carotte_card_points
+      carotted_total_points = @user.total_points - @task.carotte_card_points
+      @user.update(current_points: carotted_points, total_points: carotted_total_points)
+    end
+  end
+
   private
 
   # def dreams_params
