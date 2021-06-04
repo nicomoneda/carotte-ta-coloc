cda83eae5c7b74558c2b06202608cbb14a1c1beb
Guillaume Doré cd guillaume.dore@protonmail.com <guillaume.dore@protonmail.com>
2021-06-04 11:24:29 +0200

    create form and show for select a picture

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,8 @@ class Task < ApplicationRecord
 
   has_many :user_tasks, dependent: :destroy
   has_one  :current_user_task, -> { current_week }, class_name: 'UserTask'
+  has_one_attached :image
+
   validates :name, presence: true
   validates :description, presence: true
   validates :points, inclusion: { in: (1..200) }
