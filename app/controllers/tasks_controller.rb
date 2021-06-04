6162e9fc388298a094567d009c76cfdf8946bc1c
Guillaume Doré cd guillaume.dore@protonmail.com <guillaume.dore@protonmail.com>
2021-06-04 12:25:55 +0200

    add picture to user-tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,6 +23,6 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :description, :points, :image)
+    params.require(:task).permit(:name, :description, :points, :photo)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,6 @@ class Task < ApplicationRecord
 
   has_many :user_tasks, dependent: :destroy
   has_one  :current_user_task, -> { current_week }, class_name: 'UserTask'
-  has_one_attached :image
 
   validates :name, presence: true
   validates :description, presence: true
